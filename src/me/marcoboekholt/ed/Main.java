ccf6aef6835ed8df8c57b18fcffe1c9766543698
Marco Boekholt <marco.boekholt@hotmail.com>
2015-10-08 19:11:29 +0200

    Added commands for plugin

--- src/me/marcoboekholt/ed/Main.java
+++ src/me/marcoboekholt/ed/Main.java
@@ -1,5 +1,25 @@
 package me.marcoboekholt.ed;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import me.marcoboekholt.ed.commands.Commands;
+import me.marcoboekholt.ed.data.*;
+
+public class Main extends JavaPlugin {
+	
+	data data = new data();
+	PluginManager pm = Bukkit.getServer().getPluginManager();
+	
+	public void onEnable() {
+		getCommand("energydrinks").setExecutor(new Commands());
+		getCommand("ed").setExecutor(new Commands());
+		getServer().getLogger().info(data.PLUGIN_NAME + " has been enabled.");
+	}
+	
+	public void onDisable() {
+		getServer().getLogger().info(data.PLUGIN_NAME + "has been disabled.");
+	}
 
 }
