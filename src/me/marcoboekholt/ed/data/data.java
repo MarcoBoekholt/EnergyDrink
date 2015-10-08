259b9b8864e19d558e4a5a8925fff9d03c75b805
Marco Boekholt <marco.boekholt@hotmail.com>
2015-10-08 19:12:18 +0200

    Created data class with all message info and plugin data

---
+++ src/me/marcoboekholt/ed/data/data.java
@@ -0,0 +1,16 @@
+package me.marcoboekholt.ed.data;
+
+public class data {
+	
+	// Plugin data
+	public String PLUGIN_NAME = "EneryDrink";
+	public int PLUGIN_VERSION = 1;
+	public String AUTHOR = "Marco Boekholt";
+	
+	// Plugin commands data
+	public String HELP_TITLE = "";
+	public String COMMANDS = "/energydrink <redbull|monsterenergy|rockstar>";
+	public String NOPLAYER = "";
+	public String NOPERM = "";
+
+}
